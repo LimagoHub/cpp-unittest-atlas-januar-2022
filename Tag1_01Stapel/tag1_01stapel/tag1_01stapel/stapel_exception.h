2c7f93e3938a877523985afa8a8865df9fdb20f4
Jo Wagner <jowagner@limago.de>
2022-01-31 09:53:09 +0100

    initial commit

---
+++ Tag1_01Stapel/tag1_01stapel/tag1_01stapel/stapel_exception.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <exception>
+
+class stapel_exception :
+	public std::exception
+{
+public:
+	stapel_exception()
+	{
+	}
+
+	stapel_exception(char const* _Message)
+		: exception(_Message)
+	{
+	}
+
+	stapel_exception(char const* _Message, int i)
+		: exception(_Message, i)
+	{
+	}
+
+	stapel_exception(exception const& _Other)
+		: exception(_Other)
+	{
+	}
+};
