95bffeebc6540ccf4b6379f6bd7cab02dc678d59
Jo Wagner <jowagner@limago.de>
2022-01-31 16:30:37 +0100

    link

--- tag1_04personenService/tag1_04personenService/tag1_04personenService_test/Mockpersonen_repository.h
+++ tag1_04personenService/tag1_04personenService/tag1_04personenService_test/Mockpersonen_repository.h
@@ -3,6 +3,9 @@
 
 #include "../tag1_04personenService/personen_repository.h"
 
+
+// https://limago.my.webex.com/meet/jowagner
+
 class Mockpersonen_repository : public personen_repository {
  public:
   MOCK_METHOD(void, save, (const person& person), (override));
