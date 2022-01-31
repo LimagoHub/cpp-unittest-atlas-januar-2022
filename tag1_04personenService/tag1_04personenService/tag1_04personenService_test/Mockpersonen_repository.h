0cbeda3b6b226bcc6bdbf9003cc75453398a9d21
Jo Wagner <jowagner@limago.de>
2022-01-31 16:19:48 +0100

    repo

---
+++ tag1_04personenService/tag1_04personenService/tag1_04personenService_test/Mockpersonen_repository.h
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "../tag1_04personenService/personen_repository.h"
+
+class Mockpersonen_repository : public personen_repository {
+ public:
+  MOCK_METHOD(void, save, (const person& person), (override));
+  MOCK_METHOD(void, update, (const person& person), (override));
+  MOCK_METHOD(bool, remove, (const person& person), (override));
+  MOCK_METHOD(person, findById, (std::string id), (override));
+  MOCK_METHOD(std::vector<person>, findAll, (), (override));
+};
